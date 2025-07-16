72a1eabfbe40aa9f7e20fb4201ab03471f179818
eeyll18 <edaeylulgunay2@gmail.com>
2025-07-16 16:40:30 +0300

    add calculating winner logic

---
+++ src/utils/gameLogic.ts
@@ -0,0 +1,22 @@
+import type { Player, SquareValue } from "../types";
+
+export function calculateWinner(squares: SquareValue[]): Player | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a] as Player;
+    }
+  }
+  return null;
+}
