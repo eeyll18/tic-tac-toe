e81e38a1609418059c4d6c7428024d00203da8ea
eeyll18 <edaeylulgunay2@gmail.com>
2025-07-16 23:13:41 +0300

    enhanced design for board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,7 +9,7 @@ interface BoardProps {
 
 export const Board = ({ squares, onClick, jokerSquareIndex }: BoardProps) => {
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-3 p-4 bg-gray-900 rounded-xl shadow-lg">
       {squares.map((square, i) => (
         <Square
           key={i}
